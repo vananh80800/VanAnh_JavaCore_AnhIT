2dbce53edd15547205a804d80a63d4905e22c65a
anhnv <vananh80800@gmail.com>
2025-09-11 23:34:53 +0700

    Depploy Buoi 1

---
+++ src/Buoi1/bai2_buoi1.java
@@ -0,0 +1,18 @@
+package Buoi1;
+
+//Hoán đổi 2 biến a và b không dùng biến trung gian
+public class bai2_buoi1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Trước khi đổi: a = " + a + ", b = " + b);
+
+        // Hoán đổi không dùng biến trung gian
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Sau khi đổi: a = " + a + ", b = " + b);
+    }
+}
---
+++ src/Buoi1/bai3_buoi1.java
@@ -0,0 +1,17 @@
+package Buoi1;
+
+//Diện tích và chu vi hình chữ nhật
+public class bai3_buoi1 {
+    public static void main(String[] args) {
+        double width = 5.0;   // chiều rộng
+        double height = 10.0; // chiều dài
+
+        double dienTich = width * height;
+        double chuVi = (width + height) * 2;
+
+        System.out.println("Chiều rộng: " + width);
+        System.out.println("Chiều dài: " + height);
+        System.out.println("Diện tích hình chữ nhật: " + dienTich);
+        System.out.println("Chu vi hình chữ nhật: " + chuVi);
+    }
+}
