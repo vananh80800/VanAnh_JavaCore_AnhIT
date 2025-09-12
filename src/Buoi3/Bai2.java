862a4570b46b671a5780735ec9749472021d668d
anhnv <vananh80800@gmail.com>
2025-09-12 21:42:59 +0700

    Deploy bai tap buoi 3

--- src/Buoi3/Bai2.java
+++ src/Buoi3/Bai2.java
@@ -1,10 +1,9 @@
 package Buoi3;
 
 public class Bai2 {
-    //In ra các số chẵn trong khoảng 2 → 200
     public static void main(String[] args) {
-        int i = 2;
-        while (i <= 200) {
+        int i = 4;  // bắt đầu từ số chẵn sau 2
+        while (i < 200) {  // dừng trước 200
             System.out.print(i + " ");
             i += 2;
         }
